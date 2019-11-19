2b05cac00efd04e2e55b94a853cf0105ed803abe
santiago_oficina <exactlimon@gmail.com>
2019-11-19 18:18:58 -0500

    borrar mensajes

--- src/main/java/com/platzi/mensajes_app/MensajesDAO.java
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -62,6 +62,26 @@ public class MensajesDAO {
     }
     
     public static void borrarMensajeDB(int id_mensaje){
+        Conexion db_connect = new Conexion();
+        
+        try(Connection conexion = db_connect.get_connection())  {
+        PreparedStatement ps=null;
+        
+            try {
+                String query="DELETE FROM mensajes WHERE id_mensaje = ?";
+                ps=conexion.prepareStatement(query);
+                ps.setInt(1, id_mensaje);
+                ps.executeUpdate();
+                System.out.println("el mensaje ha sido borrado");
+            }catch(SQLException e) {
+                System.out.println(e);
+                 System.out.println("no se pudo borrar el mensaje");
+            }
+        
+            
+        }catch(SQLException e){
+            System.out.println(e);
+        }
         
     }
     
