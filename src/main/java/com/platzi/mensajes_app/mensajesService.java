a79bd5fe05847132c968a5f5593fbe9ef8b74d81
santiago_oficina <exactlimon@gmail.com>
2019-11-19 18:45:21 -0500

    actualizar mensajes

--- src/main/java/com/platzi/mensajes_app/mensajesService.java
+++ src/main/java/com/platzi/mensajes_app/mensajesService.java
@@ -37,6 +37,16 @@ public class mensajesService {
     }
     
     public static void editarMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe tu nuevo mensaje");
+        String mensaje = sc.nextLine();
+        
+        System.out.println("indica el ID del mensaje a editar");
+        int id_mensaje= sc.nextInt();
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setId_mensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizarMensajeDB(actualizacion);
         
     }
     
