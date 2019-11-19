a0c7af749dd523b91456e503b403cebd0ef55a0f
santiago_oficina <exactlimon@gmail.com>
2019-11-19 17:39:22 -0500

    Crear mensajes

--- src/main/java/com/platzi/mensajes_app/MensajesDAO.java
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -5,6 +5,10 @@
  */
 package com.platzi.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author ohmyfi
@@ -12,7 +16,24 @@ package com.platzi.mensajes_app;
 public class MensajesDAO {
     
     public static void crearMensajeDB(Mensajes mensaje){
+        Conexion db_connect = new Conexion();
         
+        try(Connection conexion = db_connect.get_connection())  {
+            PreparedStatement  ps=null;
+            try{
+                String query="INSERT INTO mensajes ( mensaje, autor_mensaje) VALUES (?,?)";
+                ps=conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+                System.out.println("mensaje creado");
+                
+            }catch(SQLException ex){
+                System.out.println(ex);
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
     
     public static void leerMensajesDB(){
--- src/main/java/com/platzi/mensajes_app/mensajesService.java
+++ src/main/java/com/platzi/mensajes_app/mensajesService.java
@@ -5,13 +5,25 @@
  */
 package com.platzi.mensajes_app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ohmyfi
  */
 public class mensajesService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
         
+        System.out.println("tu nombre");
+        String nombre = sc.nextLine();
+        
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutor_mensaje(nombre);
+        MensajesDAO.crearMensajeDB(registro);
     }
     
     public static void listarMensajes(){
